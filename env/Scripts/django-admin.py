9bfedcdcffba1b962c1c9b9edb588e35b857f43d
unknown <quoc01887156304@gmail.com>
2021-12-07 17:59:35 +0700

    First commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Dao Kien Quoc\WorkspaceForAll\Django Workspace\tasker-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
